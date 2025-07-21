d9cc74f1728d9bdecfffd3a8e3035e2c88ae9c71
viktoriia <viktoriia.safonova@hyperisland.se>
2025-07-21 13:34:33 +0200

    Update email on contacts page and resume

--- src/components/contact page/Contacts.jsx
+++ src/components/contact page/Contacts.jsx
@@ -22,7 +22,7 @@ export default function Contacts() {
           <div className="info-box">
             <i className="bx bi-envelope-at"></i>
             <h3>Email Me</h3>
-            <p>viktoriia.safonova@hyperisland.se</p>
+            <p>viktoriia.safonova.swe@gmail.com</p>
           </div>
         </div>
         <div className="col-md-6 mt-4 d-flex align-items-stretch">
